080da3259d02a081af361c20d3dcf847bba5a620
Nick Taras <nicktaras@hotmail.co.uk>
2019-12-03 16:45:22 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,9 +9,16 @@ const FILES_TO_CACHE = [
   '/offline.html',
 ];
 
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
+if (evt.request.mode !== 'navigate') {
+  // Not a page navigation, bail.
+  return;
+}
+evt.respondWith(
+    fetch(evt.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+              .then((cache) => {
+                return cache.match('offline.html');
+              });
+        })
 );
